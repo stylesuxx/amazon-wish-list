5024969ed41dab1b15402187783ee66e8b5a2746
stylesuxx <stylesuxx@gmail.com>
2015-11-09 00:20:18 +0100

    Check for wish list title

--- tests/index.js
+++ tests/index.js
@@ -2,16 +2,17 @@ var test = require('tape');
 var AmazonWishList = require('../');
 
 var testData = {
-  listID: '',
+  listID: 'NDDVVVWMJ6AN',
   cid: '',
   email: ''
 }
 
-test('Find wish list by email', function (t) {
+test('Get wish list by "wish list id"', function (t) {
   t.plan(1);
 
   var awl = new AmazonWishList('de');
-  awl.getListById().then( function(result) {
-    t.ok(result, 'Lists');
+  awl.getListById(testData.listID).then( function(result) {
+    console.log(result);
+    t.equal(result.title, 'testing', 'List title matches');
   });
 });
