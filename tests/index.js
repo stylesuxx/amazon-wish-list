41b033ca923f68f7b95ad3ec094512408d6373eb
stylesuxx <stylesuxx@gmail.com>
2015-11-09 18:38:38 +0100

    Test: Check for all items in a wish list

--- tests/index.js
+++ tests/index.js
@@ -7,7 +7,7 @@ var testData = {
   cid: 'A3ETU88UAET9K3'
 }
 
-var unpurchased = 0;
+var allItems = 0;
 
 test('Get unpurchased by wish list id', function (t) {
   t.plan(9);
@@ -16,7 +16,7 @@ test('Get unpurchased by wish list id', function (t) {
   awl.getById(testData.listID).then( function(result) {
     t.equal(result.title, 'testing', 'List title available');
     t.ok(result.items.length >= 26, 'Pagination is working');
-    unpurchase = result.items.length;
+    allItems += result.items.length;
 
     var last = result.items[result.items.length - 1];
     t.equal(last.title, 'Die Simpsons - Die komplette Season 1 (Collector\'s Edition, 3 DVDs)', 'Item title available');
@@ -36,6 +36,7 @@ test('Get purchased by wish list id', function (t) {
   awl.getById(testData.listID, 'purchased').then( function(result) {
     t.equal(result.title, 'testing', 'List title available');
     t.ok(result.items.length == 1, 'Amount matches');
+    allItems += result.items.length;
 
     var last = result.items[result.items.length - 1];
     t.equal(last.title, 'United Labels 0804201 - Simpsons Sprechender Flaschenöffner', 'Item title available');
@@ -45,6 +46,25 @@ test('Get purchased by wish list id', function (t) {
     t.equal(last.currency, 'EUR', 'Item currency available');
     t.equal(last.price, 8.99, 'Item price available');
     t.equal(last.link, 'https://amazon.de/dp/B0015GCBJG', 'Item link available');
+  });
+});
+
+test('Get all by wish list id', function (t) {
+  t.plan(9);
+
+  var awl = new AmazonWishList('de');
+  awl.getById(testData.listID, 'all').then( function(result) {
+    t.equal(result.title, 'testing', 'List title available');
+    t.ok(result.items.length == allItems, 'Amount matches');
+
+    var last = result.items[result.items.length - 1];
+    t.equal(last.title, 'Die Simpsons - Die komplette Season 1 (Collector\'s Edition, 3 DVDs)', 'Item title available');
+    t.equal(last.id, 'B00005MFO7', 'Item ID available');
+    t.equal(last.priority, 2, 'Item priority available');
+    t.equal(last.comment, 'Just a test comment.', 'Item comment available');
+    t.equal(last.currency, 'EUR', 'Item currency available');
+    t.equal(last.price, 13.99, 'Item price available');
+    t.equal(last.link, 'https://amazon.de/dp/B00005MFO7', 'Item link available');
   });
 });
 
