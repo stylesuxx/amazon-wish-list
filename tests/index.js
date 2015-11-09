e3a379353c70ed477fd5aaa16d5172e775c6ebfa
stylesuxx <stylesuxx@gmail.com>
2015-11-09 01:58:56 +0100

    Check if title matches

--- tests/index.js
+++ tests/index.js
@@ -8,11 +8,14 @@ var testData = {
 }
 
 test('Get by wish list id', function (t) {
-  t.plan(2);
+  t.plan(3);
 
   var awl = new AmazonWishList('de');
   awl.getListById(testData.listID).then( function(result) {
     t.equal(result.title, 'testing', 'List title matches');
     t.ok(result.items.length >= 26, 'Pagination is working');
+
+    var first = result.items[0];
+    t.equal(first.title, 'Die Simpsons - Die komplette Season 1 (Collector\'s Edition, 3 DVDs)', 'Item title matches');
   });
 });
